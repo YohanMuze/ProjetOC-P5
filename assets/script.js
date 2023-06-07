78aba85862d3e44867c7d166fafafece0fa45e53
Yohan Muze <yoh.nbc@hotmail.fr>
2023-06-07 15:58:35 +0200

    carousel added

--- assets/script.js
+++ assets/script.js
@@ -17,12 +17,83 @@ const slides = [
 	}
 ]
 
+//variables used for dots
+let iteration = 0;
+let dots = document.querySelector(".dots");
+let dotSelected = document.createElement("div");
+dotSelected.className = "dot_selected dot";
+let dot = document.createElement("div");
+dot.className = "dot";
+
+document.body.onload = addDot;
+//adding dots - number depend of number of slides
+function addDot() {
+	if (iteration === 0) {
+		dots.append(dotSelected);
+		iteration++;
+	}
+	while (iteration < slides.length) {
+		let clone = dot.cloneNode(true);
+		dots.append(clone);
+		iteration++;
+	}
+}
+
+//variables used for carousel
+let slideNumber = 0;
+let currentSlide = 0;
+let baliseImg = document.getElementById("current-img");
+let baliseText = document.getElementById("current-text");
+
+//functions for carousel
+function switchSlide(currentSlide) { //used to switch between slides - image + text
+	currentSlide = slides[slideNumber];
+	baliseImg.src =`./assets/images/slideshow/${currentSlide.image}`;
+	baliseText.innerHTML = `${currentSlide.tagLine}`;
+}
+
+function cloneDotNext() { //add dot after the dot_selected and remove first dot
+	let clone = dot.cloneNode(true);
+	dots.insertAdjacentElement('beforeend', clone);
+	dots.firstChild.remove();
+}
+
+function cloneDotPrev() { //add dot before dot_selected and remove last dot
+	let clone = dot.cloneNode(true);
+	dots.insertAdjacentElement('afterbegin', clone);
+	dots.lastChild.remove();
+}
+
+
 let arrowLeft = document.querySelector(".arrow_left");
 arrowLeft.addEventListener("click", () => {
-	console.log("Bouton gauche")
+	if (slideNumber === 0){
+		slideNumber = slides.length-1;
+		switchSlide(currentSlide);
+		for (let i = 0; i < slideNumber; i++) {
+			cloneDotPrev();
+		}
+		
+	} else {
+		slideNumber--;
+		switchSlide(currentSlide);
+		cloneDotNext();
+	}
 });
 
 let arrowRight = document.querySelector(".arrow_right");
 arrowRight.addEventListener("click", () => {
-	console.log("Bouton droit")
+	console.log("Bouton droit");
+	if (slideNumber === slides.length-1){
+		slideNumber = 0;
+		switchSlide(currentSlide);
+		for (let i = 0; i < slides.length-1; i++) {
+			cloneDotNext();
+		}
+	} else {
+		slideNumber++;
+		switchSlide(currentSlide);
+		cloneDotPrev();
+	}
 });
+
